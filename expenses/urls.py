a3711352f029dd6c864183bd6a39ef21b80e393b
Hack Reactor <>
2022-03-24 08:09:59 -0500

    Remove unnecessary import.

--- expenses/urls.py
+++ expenses/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from audioop import reverse
 from django.contrib import admin
 from django.urls import path, include, reverse_lazy
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("receipts/", include("receipts.urls")),
     path("", RedirectView.as_view(url=reverse_lazy("home"))),
