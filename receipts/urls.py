156e0b045bf4ed386da7896e11efd7462ba65261
Jordan Tran <thai.jordan@gmail.com>
2022-10-20 12:58:17 -0700

    Feature 12 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import receipt_list, create_receipt
+from .views import receipt_list, create_receipt, account_list, category_list
 
 
 urlpatterns = [
     path("", receipt_list, name="home"),
     path("create/", create_receipt, name="create_receipt"),
+    path("accounts/", account_list, name="account_list"),
+    path("categories/", category_list, name="category_list")
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Receipt
+from .models import Receipt, ExpenseCategory, Account
 from django.contrib.auth.decorators import login_required
 from receipts.forms import ReceiptForm
 
@@ -39,3 +39,21 @@ def create_receipt(request):
         "form": form,
     }
     return render(request, "receipts/create.html", context)
+
+
+@login_required
+def category_list(request):
+    categories = ExpenseCategory.objects.filter(owner=request.user)
+    context = {
+        "categories": categories,
+    }
+    return render(request, "categories/list.html", context)
+
+
+@login_required
+def account_list(request):
+    accounts = Account.objects.filter(owner=request.user)
+    context = {
+        "accounts": accounts,
+    }
+    return render(request, "accounts/list.html", context)
